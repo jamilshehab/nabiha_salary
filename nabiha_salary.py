a5c8a5934f52782132508cfaba519252f2a3a058
jamilshehab <jamilshehab>
2025-02-18 16:26:18 +0200

    savings rents and electricity consumptions added

--- nabiha_salary.py
+++ nabiha_salary.py
@@ -1,3 +1,7 @@
 #Create a user inputs
 nabiha_salary_of_the_month=int(input("Nabiha enter your salary of the month : "))
 month_name=input("enter your month name to store your salary for : ")
+your_saving_nabiha=int(input("Nabiha enter your saving  : "))
+your_rent_nabiha=int(input("Nabiha enter your rent : "))
+your_electricity_consumption=int(input("Nabiha enter your electricity consumption : "))
+
