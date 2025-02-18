6704b3d5866bb340fc24d852fc59faa70a42f0b0
jamilshehab <jamilshehab>
2025-02-18 19:42:15 +0200

    calculate the percentage of saving , rating and electricity

--- nabiha_salary.py
+++ nabiha_salary.py
@@ -5,6 +5,7 @@ your_saving_nabiha=int(input("Nabiha enter your saving  : "))
 your_rent_nabiha=int(input("Nabiha enter your rent : "))
 your_electricity_consumption=int(input("Nabiha enter your electricity consumption : "))
 
+#calculate the percentage of saving , rating and electricity
 nabiha_total_saving=your_saving_nabiha / nabiha_salary_of_the_month * 100 
 nabiha_total_rent=your_saving_nabiha / nabiha_salary_of_the_month * 100 
 nabiha_total_electricity_consumption=your_saving_nabiha / nabiha_salary_of_the_month * 100 
