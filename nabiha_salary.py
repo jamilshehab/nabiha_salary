2021ef210a36b2ab6ea146437f15e903c441169a
jamilshehab <jamilshehab>
2025-02-19 20:44:51 +0200

    code refactored part 1

--- nabiha_salary.py
+++ nabiha_salary.py
@@ -1,29 +1,31 @@
+number_of_month=12
+while number_of_month:
+
 #Create a user inputs
-nabiha_salary_of_the_month=int(input("Nabiha enter your salary of the month : "))
-month_name=input("enter your month name to store your salary for : ")
-your_saving_nabiha=int(input("Nabiha enter your saving  : "))
-your_rent_nabiha=int(input("Nabiha enter your rent : "))
-your_electricity_consumption=int(input("Nabiha enter your electricity consumption : "))
-is_additonal_amount=True
-additional_amount=50
+      nabiha_salary_of_the_month=int(input("Nabiha enter your salary of the month : "))
+      month_name=input("enter your month name to store your salary for : ")
+      your_saving_nabiha=int(input("Nabiha enter your saving  : "))
+      your_rent_nabiha=int(input("Nabiha enter your rent : "))
+      your_electricity_consumption=int(input("Nabiha enter your electricity consumption : "))
+      additional_amount=150
 #calculate the percentage of saving , rating and electricity
-nabiha_total_saving=your_saving_nabiha / nabiha_salary_of_the_month * 100 
-nabiha_total_rent=your_rent_nabiha / nabiha_salary_of_the_month * 100 
-nabiha_total_electricity_consumption=your_electricity_consumption / nabiha_salary_of_the_month * 100 
+      nabiha_total_saving=your_saving_nabiha / nabiha_salary_of_the_month * 100 
+      nabiha_total_rent=your_rent_nabiha / nabiha_salary_of_the_month * 100 
+      nabiha_total_electricity_consumption=your_electricity_consumption / nabiha_salary_of_the_month * 100 
  
-total_amount=nabiha_total_saving + nabiha_total_rent + nabiha_total_electricity_consumption
+      total_amount=nabiha_total_saving + nabiha_total_rent + nabiha_total_electricity_consumption
 
-remainder_salary=nabiha_salary_of_the_month % total_amount
+      remainder_salary=nabiha_salary_of_the_month % total_amount
 
-total_yearly_rent=nabiha_total_rent + nabiha_total_electricity_consumption * 12
+      total_yearly_rent=nabiha_total_rent + nabiha_total_electricity_consumption * 12
 
 #nabiha total salary of the month times 2 just for fun
-nabiha_double_total_salary=nabiha_salary_of_the_month ** 2
+      nabiha_double_total_salary=nabiha_salary_of_the_month ** 2
 
 #display a condition 
-if is_additonal_amount:
-   nabiha_additional_total_amount=additional_amount / total_amount
-   print(f"Nabiha Additional Amount is :  {nabiha_additional_total_amount}")
+   #  if is_additonal_amount:
+   #       nabiha_additional_total_amount=additional_amount / total_amount
+   #       print(f"Nabiha Additional Amount is :  {nabiha_additional_total_amount}")
 
 print(f"Nabiha Salary of the  Month is : {nabiha_salary_of_the_month} $")
 print(f"The Month Name is  : {month_name}")
